a01592b7e747cc434773a6cdf56b2fcfb9969a9c
alphaRaWaY <1328178536@qq.com>
2025-07-15 11:27:15 +0800

    调整目录

---
+++ nowcoder/r1/A/v1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define inc(i,a,b) for(int i(a);i<=b;i++)
+#define dec(i,a,b) for(int i(a);i>=b;i--)
+void solve()
+{
+   
+}
+signed main()
+{
+    ios::sync_with_stdio(false);
+    int _=1;
+    // cin>>_;
+    while(_--)solve();
+}
